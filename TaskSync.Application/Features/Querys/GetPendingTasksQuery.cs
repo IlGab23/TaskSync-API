eb91b6fbfae374017f5340c3090640f4c90d7ec1
Gabriele Gullì <magapano89@gmail.com>
2026-07-14 12:00:06 +0200

    Fix: Aggiornato valore di ritorno in GetPendingTasksQuery : IRequest<> da GetPendingTasksQuery a GetPendingTasksQueryOutput

--- TaskSync.Application/Features/Querys/GetPendingTasksQuery.cs
+++ TaskSync.Application/Features/Querys/GetPendingTasksQuery.cs
@@ -5,6 +5,6 @@ using TaskSync.Domain.Entities.ValueObjects;
 using TaskSync.Domain.ResultPattern;
 namespace TaskSync.Application.Features.Querys;
 
-public record GetPendingTasksQuery(Guid UserId) : IRequest<Result<GetPendingTasksQuery>>;
+public record GetPendingTasksQuery(Guid UserId) : IRequest<Result<GetPendingTasksQueryOutput>>;
 public record GetPendingTasksQueryOutput(List<TaskData> Tasks);
 public record TaskData(string Title, string Description, DateTimeOffset Expiry, bool IsCompleted, DateTimeOffset CreatedAtUtc);
