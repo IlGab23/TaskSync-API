f73806ba1f8336a6bda8083d8763631768ac25fa
Gabriele Gullì <magapano89@gmail.com>
2026-07-14 11:46:52 +0200

    Fix: Sostituito DueDateUtc con DateTimeOffset in GetPendingTasksQuery

--- TaskSync.Application/Features/Querys/GetPendingTasksQuery.cs
+++ TaskSync.Application/Features/Querys/GetPendingTasksQuery.cs
@@ -1,8 +1,10 @@
 using MediatR;
 using Microsoft.VisualBasic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using TaskSync.Domain.Entities.ValueObjects;
 using TaskSync.Domain.ResultPattern;
 namespace TaskSync.Application.Features.Querys;
 
 public record GetPendingTasksQuery(Guid UserId) : IRequest<Result<GetPendingTasksQuery>>;
 public record GetPendingTasksQueryOutput(List<Task> Tasks);
-public record Task(string Title, string Description, DueDate Expiry, bool IsCompleted, DateTimeOffset CreatedAtUtc);
+public record Task(string Title, string Description, DateTimeOffset Expiry, bool IsCompleted, DateTimeOffset CreatedAtUtc);
